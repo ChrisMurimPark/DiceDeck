91f3e2b76f9e923a5fc3fa0f2666668bf28755fb
Chris Park <chris.murim.park@gmail.edu>
2020-05-25 11:27:45 -0500

    Initial Xcode project commit.
    Implements DiceDeck version 0.

---
+++ DiceDeck/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiceDeck
+//
+//  Created by Chris (Murim) Park on 5/24/20.
+//  Copyright © 2020 Chris (Murim) Park. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
